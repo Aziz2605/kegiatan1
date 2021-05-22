250e630ac980930c70f143da4747c45dd2d658bf
iqbalwag <achiqbalp@gmail.com>
2021-05-22 10:51:30 +0700

    default

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<div class="card" style="max-width: 18rem;">
+  <img src="images\aku.jpg" size="small" class="card-img-top" style="max-width: 18rem;>
+  <div class="card-body">
+    <p class="card-text">Nama     : Achmad Iqbal Prasetya</p>
+    <p class="card-text">NBI      : 1461900069</p>
+    <p class="card-text">Kontak   : 085850775655</p>
+    <p class="card-text">Email    : achiqbalp@gmail.com</p>
+    <p class="card-text">IG       : Ai</p>
+    <p class="card-text">Facebook : Iqbal</p>
+    <a href = "/">Back</href>
+  </div>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+</head>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {border: 2px solid #000000; background-color: #FFFFFF; color: black;}
+.button2 {background-color: #000000; color: white;}
+</style>
+<body>
+	<div class="container-md" align="center">
+  		<h1>HOME</h1>
+  		<td align="center"><button onclick="document.location='/artikel'" class="button button1">Artikel</button>&emsp;<button onclick="document.location='/contact'" class="button button2">Contact US</button></td>
+	</div>
+</body>
+</html>
