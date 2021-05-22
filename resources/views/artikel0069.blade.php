fa5bef56686f8a6b7e4655b95bc06d0dcc6a76ba
Aziz2605 <84568798+Aziz2605@users.noreply.github.com>
2021-05-22 13:11:42 +0700

    Delete artikel0069.blade.php

--- resources/views/artikel0069.blade.php
+++
@@ -1,4 +0,0 @@
-<h3>Anda pasti tahu bahasa pemrograman PHP? Laravel adalah satu-satunya framework yang membantu Anda untuk memaksimalkan penggunaan PHP di dalam proses pengembangan website. 
-
-PHP menjadi bahasa pemrograman yang sangat dinamis, tapi semenjak adanya Laravel, dia menjadi lebih powerful, cepat, aman, dan simpel. Setiap rilis versi terbaru, Laravel  selalu memunculkan teknologi baru di antara framework PHP lainnya.</h3>
-<a href = "/">Back</href>
