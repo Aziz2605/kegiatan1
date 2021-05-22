a4f7fc04d67420dd2857aa3d3cb45adf15a884d7
Aziz2605 <84568798+Aziz2605@users.noreply.github.com>
2021-05-22 13:32:32 +0700

    Add files via upload

---
+++ resources/views/contactus0094.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact US</title>
+</head>
+<body background="image/tes6.jpg">
+    <img src="image/aku.jpg" size="small" class="card-img-top" style="max-width: 18rem";>
+    <p>Nama : Ahmad Abdul Aziz</p>
+    <p>NBI : 1461900094</p>
+    <p>Kontak(WA) : 083831408534</p>
+    <p>Email : abdulkeren08@gmail.com</p>
+    <p>Facebook : Ahmad Abdul Aziz</p>
+    <p>Instagram  : ahmad_abdul_aziz05
+</body>
+</html>
---
+++ resources/views/home0094.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HOME</title>
+</head>
+<body background="image/tes6.jpg">
+<div id="header" class="header">
+        <div id="seacrh">
+</div>
+    <h1>Ini Adalah Halaman Home</p>
+    <li class="first"><a href="/artikel0094">Artikel</a></li>
+    <li class="first"><a href="/contactus0094">Contact US</a</li> 
+</body>
+</html>
